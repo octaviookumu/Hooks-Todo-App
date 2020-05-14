fbbdcee05827499f518ab9a8778de7cf0f7aaa27
Octavian Okumu <48207524+octaviookumu@users.noreply.github.com>
2020-05-15 00:45:13 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import Todo from './components/Todo';
+import TodoForm from './components/TodoForm';
+import './App.css';
+
+function App() {
+	const [ todos, setTodos ] = useState([
+		{
+			text: 'Learn about React',
+			isCompleted: false
+		},
+		{
+			text: 'Meet friend for lunch',
+			isCompleted: false
+		}
+	]);
+
+	const addTodo = (text) => {
+		const newTodos = [ ...todos, { text } ];
+		setTodos(newTodos);
+	};
+
+	const completeTodo = (index) => {
+		const newTodos = [ ...todos ];
+		newTodos[index].isCompleted = true;
+		setTodos(newTodos);
+	};
+
+	const removeTodo = (index) => {
+		const newTodos = [ ...todos ];
+		newTodos.splice(index, 1);
+		setTodos(newTodos);
+	};
+
+	return (
+		<div className="app">
+			<div className="todo-list">
+				{todos.map((todo, index) => {
+					return (
+						<Todo
+							key={index}
+							index={index}
+							todo={todo}
+							completeTodo={completeTodo}
+							removeTodo={removeTodo}
+						/>
+					);
+				})}
+				<TodoForm addTodo={addTodo} />
+			</div>
+		</div>
+	);
+}
+
+export default App;
